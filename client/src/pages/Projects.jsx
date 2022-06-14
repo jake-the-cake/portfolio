4b9bab1a56e01aadf09556c0f40c84d3c3b3b148
Jakey Wakey <askjake331@gmail.com>
2022-06-14 04:38:37 -0400

    dynamic title changing

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import SetTitles from '../scripts/SetTitles'
 
-const Projects = () => {
+const Projects = (props) => {
+	SetTitles(props.title)
+
 	return (
 		<div>Projects</div>
 	)
