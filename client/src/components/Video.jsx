e9d76070ebbbcf2fc632abecd523175c3de00b3c
jake-the-cake <askjake331@gmail.com>
2022-06-28 07:45:08 -0400

    add video

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,7 +1,8 @@
 export const Video = (props={}) => {
 	return (
-		<div className="bg-secondary d-flex justify-content-center align-items-center" style={{height:'35vh'}}>
-			Video
+		<div className="bg-secondary d-flex flex-column gap-4 justify-content-center align-items-center" style={{minHeight:'35vh', paddingBottom:'2rem'}}>
+			<div className="project-card-title text-light">About Me</div>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/tQbSTMybY-c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
 		</div>
 	)
 }
